ec935e58cbf28382baf5c99c9291ee1bf81c65e8
IDMendis <dinushimendisxp@gmail.com>
2026-03-10 19:10:43 +0530

    modified the ui

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -76,7 +76,8 @@ function Dashboard() {
           fontWeight: 400,
           letterSpacing: "2px",
           textAlign: "center",
-          textShadow: "2px 2px 4px rgba(0,0,0,0.5)"
+          textShadow: "2px 2px 4px rgba(0,0,0,0.5)",
+          marginBottom: "50px"
         }}
       >
         🌱 Smart Plant Monitoring Dashboard
